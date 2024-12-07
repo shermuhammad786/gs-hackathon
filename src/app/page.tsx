1119ce4dc9195e3e84e725ba02d0180b1b907317
shermuhammad786 <sherabro141@gmail.com>
2024-12-07 23:07:09 +0500

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Button className="text-4xl p-12 text-red-600 bg-amber-600">Shadcn</Button>
+      <FontAwesomeIcon className="w-10" icon={faHeart} />
+      <FontAwesomeIcon className="w-10" icon={faHeart} />
+      <FontAwesomeIcon className="w-10" icon={faHeart} />
+      <FontAwesomeIcon className="w-10" icon={faHeart} />
+    </div>
+  );
+}
